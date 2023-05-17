64fbc3ef388d19985871d80d9a2e225adfe5b276
IsmailSabryy <124688766+IsmailSabryy@users.noreply.github.com>
2023-05-17 22:43:16 +0300

    Update PL.h

--- PL.h
+++ PL.h
@@ -4,17 +4,25 @@
 class PL {
 private:
     COR corpus;
-    double percentage;
-    int matchingCount;
-    int totaloffile;
-    int timetaken;
+    double rpercentage;
+    double bpercentage;
+    int rmatchingCount;
+    int bmatchingCount;
+    int rtotaloffile;
+    int btotaloffile;
+    int btimetaken;
+    int rtimetaken;
+    bool repeated;
 
 public:
-    PL(COR& corpus) : corpus(corpus), percentage(0.0), matchingCount(0), totaloffile(0),timetaken(0) {}
-    int gettimetaken();
+    PL(COR& corpus) : corpus(corpus), rpercentage(0.0), bpercentage(0.0), rmatchingCount(0), bmatchingCount(0),
+        rtotaloffile(0), btotaloffile(0), btimetaken(0), rtimetaken(0), repeated(false) {}
+    int rgettimetaken();
+    int bgettimetaken();
     double compareUsingBruteForce(string filename);
     double compareUsingRabinKarp(string filename);
-    double calculatePercentage();
+    double rcalculatePercentage();
+    double bcalculatePercentage();
     vector<string> extractWords( const string& line);
 
     
